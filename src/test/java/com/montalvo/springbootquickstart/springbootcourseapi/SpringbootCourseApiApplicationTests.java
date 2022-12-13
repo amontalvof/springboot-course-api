7e5fa8a8b0a78d8e6f2e43659baa061ff617d001
amontalvof <amontalvof92@gmail.com>
2022-12-13 11:59:49 -0600

    make it better

---
+++ src/test/java/com/montalvo/springbootquickstart/springbootcourseapi/SpringbootCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montalvo.springbootquickstart.springbootcourseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
